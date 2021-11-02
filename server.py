c91b12a3ca6334aec9fbeebfae373d813bdbb5b0
maidaxabdi <maidaxabdi@gmail.com>
2021-11-02 11:00:06 -0500

    Completed Database Relations

---
+++ server.py
@@ -0,0 +1,27 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    # DebugToolbarExtension(app)
+
+    app.run(host="0.0.0.0", debug=True)
